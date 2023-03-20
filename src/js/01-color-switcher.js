c0f438a5e0aeed090d49fbe66de758b36dd3d588
Julik85 <mironuk@ukr.net>
2023-03-20 18:15:39 +0200

    ch2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,8 @@
 const backgroundColor = document.querySelector('body');
 const btnStart = document.querySelector('[data-start]');
 const btnStop = document.querySelector('[data-stop]');
+let timerId = null;
+
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
